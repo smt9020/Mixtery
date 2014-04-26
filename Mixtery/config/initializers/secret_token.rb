9f463e28da899e9d15508356856a355792c19f59
Shannon Trudeau <smt9020@rit.edu>
2014-04-26 13:20:51 -0400

    The original structure

---
+++ Mixtery/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mixtery::Application.config.secret_key_base = '745999f8ed471e2ac8427bcfed87872df99bbd8b7dcbd577cc09172a90a0bb58f8c52f2854584273db78359a9122e58c9fc11c75cd171bf5b9535137f0cbe1ed'
---
+++ Mixtery/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mixtery::Application.config.session_store :cookie_store, key: '_Mixtery_session'
