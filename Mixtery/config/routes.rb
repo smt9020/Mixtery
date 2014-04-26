2b228fb18728050edba474d903f8a42f3642c376
hollyhastings <hhastings111@gmail.com>
2014-04-26 15:26:59 -0400

    Implement playing audio on tracks page

--- Mixtery/config/routes.rb
+++ Mixtery/config/routes.rb
@@ -1,6 +1,7 @@
 Mixtery::Application.routes.draw do
   resources :tracks
 
+  root :to => "tracks#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
